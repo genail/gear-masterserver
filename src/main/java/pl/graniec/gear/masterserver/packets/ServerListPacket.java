4576ce975a46a2286c27917cd064781caf559b16
Piotr Korzuszek <piotr.korzuszek@gmail.com>
2010-03-30 18:30:30 +0200

    Create ServerListPacket

---
+++ src/main/java/pl/graniec/gear/masterserver/packets/ServerListPacket.java
@@ -0,0 +1,73 @@
+package pl.graniec.gear.masterserver.packets;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import pl.graniec.gear.masterserver.NetGameEvent;
+import pl.graniec.gear.masterserver.NetGameEventValue;
+import pl.graniec.gear.masterserver.exceptions.DataCorruptedException;
+import pl.graniec.gear.masterserver.registry.RegistryEntry;
+
+public final class ServerListPacket implements Packet {
+
+	// static fields
+	
+	public static final String NAME = "SERVERLIST";
+	
+	private static final int HEADER_SIZE = 2;
+	
+	private static final int ENTRY_SIZE = 4;
+	
+	
+	// non-static fields
+	
+	private final Set<RegistryEntry> entriesSet = new HashSet<RegistryEntry>();
+	
+	public final void addRegistryEntry(RegistryEntry entry) {
+		entriesSet.add(entry);
+	}
+	
+	@Override
+	public NetGameEvent buildEvent() {
+		
+		final int valuesSize = HEADER_SIZE + (ENTRY_SIZE * entriesSet.size());
+		final NetGameEventValue<?>[] values = new NetGameEventValue<?>[valuesSize];
+		
+		writeEntrySize(values);
+		writeEntriesCount(values);
+		
+		int i = HEADER_SIZE;
+		
+		for (final RegistryEntry entry : entriesSet) {
+			writeEntry(values, i, entry);
+			i += ENTRY_SIZE;
+		}
+		
+		return new NetGameEvent(NAME, values);
+	}
+
+	private void writeEntry(
+			NetGameEventValue<?>[] values,
+			int offset,
+			RegistryEntry entry
+	) {
+		values[offset++] = new NetGameEventValue<String>(entry.getServerAddr());
+		values[offset++] = new NetGameEventValue<Integer>(entry.getServerPort());
+		values[offset++] = new NetGameEventValue<String>(entry.getServerName());
+		values[offset++] = new NetGameEventValue<String>(entry.getCurrentMapName());
+	}
+
+	private void writeEntriesCount(NetGameEventValue<?>[] values) {
+		values[1] = new NetGameEventValue<Integer>(entriesSet.size());
+	}
+
+	private void writeEntrySize(NetGameEventValue<?>[] values) {
+		values[0] = new NetGameEventValue<Integer>(ENTRY_SIZE);
+	}
+
+	@Override
+	public void parseEvent(NetGameEvent event) throws DataCorruptedException {
+		throw new RuntimeException("not expected to be used");
+	}
+
+}
