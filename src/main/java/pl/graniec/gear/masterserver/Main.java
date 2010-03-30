a2d0f1a39206dd6b0320a8d3c9981a390a2e6679
Piotr Korzuszek <piotr.korzuszek@gmail.com>
2010-03-30 12:32:00 +0200

    Rename Prog.java to Main.java

--- src/main/java/pl/graniec/gear/masterserver/Main.java
+++ src/main/java/pl/graniec/gear/masterserver/Main.java
@@ -3,7 +3,7 @@ package pl.graniec.gear.masterserver;
 
 
 
-public class Prog{
+public class Main {
 	public static void main(String[] args) throws Exception{
 		final MasterServer masterServer = new MasterServer();
 		masterServer.run();
